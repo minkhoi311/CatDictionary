5ff9060dadede5c0e75343fda9ba59bc73c9c9b9
minh khoi <minkhoi.dev@gmail.com>
2025-03-12 14:26:19 +0700

    Fix add form + Add base form

--- AddForm.cs
+++ AddForm.cs
@@ -10,12 +10,16 @@ using System.Windows.Forms;
 
 namespace Dictionary
 {
-    public partial class AddForm: Form
+    public partial class AddForm: BaseForm
     {
         public AddForm()
         {
             InitializeComponent();
         }
-        
+
+        private void AddForm_Load(object sender, EventArgs e)
+        {
+            ApplyButtonDesign(new Button[] { btnDone }, 30);
+        }
     }
 }
