cb0b81d6a32fe27657016aa8c96d5d64fec7740a
minh khoi <minkhoi.dev@gmail.com>
2025-03-15 21:37:20 +0700

    push lại của Mỹ Ân

--- AddForm.cs
+++ AddForm.cs
@@ -12,14 +12,47 @@ namespace Dictionary
 {
     public partial class AddForm: BaseForm
     {
+        public string WordValue { get;private set; }
+        public string IpaValue { get;private set; }
+        public string MeanValue { get; private set; }
+        public string Ex1Value { get; private set; }
+        public string Ex2Value { get; private set; }
+        public string Ex3Value { get; private set; }
         public AddForm()
         {
             InitializeComponent();
         }
-
+        
         private void AddForm_Load(object sender, EventArgs e)
         {
             ApplyButtonDesign(new Button[] { btnDone }, 30);
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtWord.Text) || string.IsNullOrWhiteSpace(txtDefinition.Text))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin từ vựng và nghĩa!", "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc muốn thêm từ vựng này vào danh sách?", "Xác nhận",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                WordValue = txtWord.Text.Trim();
+                IpaValue = txtIPA.Text.Trim();
+                MeanValue = txtDefinition.Text.Trim();
+                Ex1Value = txtEx1.Text.Trim();
+                Ex2Value = txtEx2.Text.Trim();
+                Ex3Value = txtEx3.Text.Trim();
+
+                MessageBox.Show("Thêm từ vựng thành công!", "Thông báo!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
         }
     }
 }
